59669f0aa82e51512bcff0d2effd58a8538da8d4
Iraka-C <iraka@qq.com>
2020-05-16 00:52:49 +0900

    create brushtip render structure

--- gl/javascripts/renderer-gl-brushtip.js
+++ gl/javascripts/renderer-gl-brushtip.js
@@ -1,7 +1,13 @@
 class GLBrushtipRenderer extends GLBrushRenderer{
 	constructor(renderer){
 		super(renderer);
+		this._initBrushtipGenerationProgram();
 	}
 
-	
+	/**
+	 * Generate GLProgram that renders a brushtip texture from given imagedata source
+	 */
+	_initBrushtipGenerationProgram(){
+
+	}
 }
